40344e917f0d08974651430d6fd817fd8733023c
i零析 <114292100@qq.com>
2022-12-02 14:34:50 +0800

    引入vue-router及axios

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
+import HelloWorld from "../components/HelloWorld.vue";
+
+const routes = [
+    {
+        path: '/hello',
+        redirect: '/hello/hello',
+        name: 'HelloWorld',
+        children: [
+            {
+                path: '/hello',
+                component: HelloWorld,
+                name: 'HelloWorld',
+            }
+        ]
+    }
+]
+const router = createRouter({
+    routes, history: createWebHistory()
+})
+export default router;
