67260135377b17f9b58abdde545227145e1096fe
i0xi <114292100@qq.com>
2023-08-07 10:02:26 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
-import Home from "../views/HomePage.vue";
+import {createRouter, createWebHistory} from "vue-router";
+import HomePage from "../views/HomePage.vue";
 
 const routes = [
     {
         path: '/',
         name: '光通',
-        component: Home,
+        component: HomePage,
     }
 ]
 const router = createRouter({
